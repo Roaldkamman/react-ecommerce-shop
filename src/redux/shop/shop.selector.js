72a598901990b07902728a0192861a3069933dae
Roaldkamman <55511890+Roaldkamman@users.noreply.github.com>
2020-08-26 16:15:45 -0400

    implemented Data Normalization on the shop.data by converting it into an object, and edited shop.selector to work with those changes.

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -22,10 +14,3 @@ export const selectCollection = memoize((collectionUrlParam) =>
     (collections) => collections[collectionUrlParam]
   )
 );
-
-// if need to memoize
-//export const selectCollection = collectionUrlParam => 
- // createSelector(
-   //   [selectCollections],
-     // collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
- // )
